29889026109774d456df3fa93da6c4ccbd6d8ae9
suzyi <1274865991@qq.com>
2021-12-16 13:36:47 +0800

    Add files via upload

---
+++ py-files/sortListByAxis.py
@@ -0,0 +1,11 @@
+if __name__=="__main__":
+    scores = [0.34, 0.39, 0.35, 0.35, 0.42]
+    names = ['731.png', '293.png', '119', '302.png', '672.png']
+    assert len(scores)==len(names)
+    
+    score2name = []
+    for name, score in zip(names, scores):
+        score2name.append([name, score])
+    print(score2name)
+    score2name_ordered = sorted(score2name, key=lambda x: x[1], reverse=True)
+    print(score2name_ordered)
